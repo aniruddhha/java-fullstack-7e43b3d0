8a1ff7440c4aa3ded9c03852711350429ba09ab2
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-02 10:41:33 +0530

    procedure oriented programming

---
+++ 07-day/oop/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    static void createPerson() {
+        String name1 = "abc";
+        int age1 = 10;
+        double sal1 = 30;
+
+        String name2 = "abc";
+        int age2 = 30;
+        double sal2 = 30;
+
+        String name3 = "abc";
+        int age3 = 50;
+        double sal3 = 30;
+    }
+
+    static void determineDob() {
+        int birthYear1 = 2023 - 10;
+        int birthYear2 = 2023 - 30;
+        int birthYear3 = 2023 - 50;
+    }
+
+    public static void main(String[] args) {
+        String name1 = "abc";
+        int age1 = 10;
+        double sal1 = 30;
+
+        String name2 = "abc";
+        int age2 = 30;
+        double sal2 = 30;
+
+        String name3 = "abc";
+        int age3 = 50;
+        double sal3 = 30;
+
+        int birthYear1 = 2023 - 10;
+        int birthYear2 = 2023 - 30;
+        int birthYear3 = 2023 - 50;
+
+        createPerson();
+
+        determineDob();
+    }
+}

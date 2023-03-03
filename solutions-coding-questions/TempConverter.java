8e9da1f534f5ad17e3a8e9807abc61b1b848906d
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-03 16:01:04 +0530

    coding questions : cels to fah

---
+++ solutions-coding-questions/TempConverter.java
@@ -0,0 +1,23 @@
+public class TempConverter {
+    
+    // F = (C * 9/5) + 32.
+
+    private double tempCles;
+    private double tempFah;
+    
+    public TempConverter(double tempCles) {
+        this.tempCles = tempCles;
+        this.tempFah = 0.0d;
+    }
+
+    public double clesToFah() {
+        return tempFah = (tempCles * (9/5)) + 32;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        TempConverter converter = new TempConverter(90);
+        System.out.println("Temp in Fah "+ converter.clesToFah());
+    }
+}

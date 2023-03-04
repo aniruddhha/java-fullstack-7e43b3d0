1b9c17b3489dfdef69e690a3808abba5bef9e9df
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-04 12:14:07 +0530

    loops : basics of for, while, do-while

---
+++ 09-day/loops/LoopsMain.java
@@ -0,0 +1,41 @@
+
+class Loops {
+    public void basicFor() {
+        for(int i = 0; i < 10; i++ ) {
+            System.out.println(i);
+        }
+    }
+    
+    public void basicWhile() {
+        int i = 0;
+        while(true) {
+            if(i > 10) break;
+            else {
+                i++;
+                System.out.println(i);
+                continue; 
+            }
+            // if you dont write continue still it okay
+            // just for explaining, i have written
+        }
+    }
+
+    public void basicDoWhile() {
+        int i = 0;
+
+        do {
+            if(i > 10) break;
+            i++;
+            System.out.println(i);
+        }while(i < 10);
+    }
+}
+
+public class LoopsMain {
+    public static void main(String[] args) {
+        Loops lp = new Loops();
+        // lp.basicFor();
+        // lp.basicWhile();
+        lp.basicDoWhile();
+    }
+}

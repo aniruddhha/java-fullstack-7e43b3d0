0f610cefad8e8b9ae35862e46046ad82410b50a8
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-09 12:10:04 +0530

    exception handing : multi catch blocks

--- 12-day/ExceptionHandlings/src/com/ani/ExceptionDemo.java
+++ 12-day/ExceptionHandlings/src/com/ani/ExceptionDemo.java
@@ -1,26 +1,31 @@
 package com.ani;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExceptionDemo {
-    
+
     public void demo1() {
         int op1 = 10;
         int op2 = 0;
 
         // addition op
         int res1 = op1 + op2;
-        System.out.println("Addition "+ res1);
+        System.out.println("Addition " + res1);
 
         // subtraction
         int res2 = op1 - op2;
-        System.out.println("Subtraction "+ res2);
+        System.out.println("Subtraction " + res2);
 
         // multiplication
         int res3 = op1 * op2;
-        System.out.println("Multiplication "+ res3);
+        System.out.println("Multiplication " + res3);
 
         // division
         int res4 = op1 / op2; // 10/0 ArithmaticException
-        System.out.println("Division "+ res4);
+        System.out.println("Division " + res4);
     }
 
     public void demo2() {
@@ -29,7 +34,7 @@ public class ExceptionDemo {
     }
 
     public void demo3() {
-        String str = null; 
+        String str = null;
         str.length(); // NullPointerException
     }
 
@@ -38,17 +43,30 @@ public class ExceptionDemo {
         try {
             // expected event is, it will load CarUtil class
             // unexpected is CarUtil is not available
-            Class.forName("com.ani.CarUtil");   // 🫳 throw new ClassNotFoundException(); 👈 
-        } catch (Exception e/*🤌*/) {
-            System.out.println("❌ CarUtil class is not available");
+            Class.forName("com.ani.CarUtilccc"); // 🫳 throw new ClassNotFoundException(); 👈
+            
+            int a = 10 / 0; // 🫳 throw new ArithmaticException(); 👈
+
+            // forget about what file reader is, right now.
+            FileReader fr = new FileReader("ghfhfg.txt"); // 🫳 throw new FileNotFoundException(); 👈
+        } catch (FileNotFoundException e /* 🤌 */) {
+            System.out.println("❌ File not found");
+        } catch (ClassNotFoundException e /* 🤌 */) {
+            System.out.println("❌ CarUtil not found");
+        } catch (ArithmeticException e/* 🤌 */) {
+            System.out.println("❌ You are dividing by zero");
+        } catch (Exception e/* 🤌 */) {
+            System.out.println("❌ I didnt find matching Exception");
+        } catch (Throwable t /* 🤌 */) {
+            System.out.println("❌ I find Generic Exception");
         } finally {
             System.out.println("⌛️ I will execute in all cases");
         }
     }
 
     public void demo5() {
-        while(true) {
-            while(true) {
+        while (true) {
+            while (true) {
                 new String();
             }
         }

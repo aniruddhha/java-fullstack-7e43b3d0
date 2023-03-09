04543f51390b6c7efd53c4cab04a883b39bd10c3
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-09 10:38:35 +0530

    exceptions : types and rutime/compile time exceptions

---
+++ 12-day/ExceptionHandlings/src/com/ani/ExceptionDemo.java
@@ -0,0 +1,39 @@
+package com.ani;
+
+public class ExceptionDemo {
+    
+    public void demo1() {
+        int op1 = 10;
+        int op2 = 0;
+
+        // addition op
+        int res1 = op1 + op2;
+        System.out.println("Addition "+ res1);
+
+        // subtraction
+        int res2 = op1 - op2;
+        System.out.println("Subtraction "+ res2);
+
+        // multiplication
+        int res3 = op1 * op2;
+        System.out.println("Multiplication "+ res3);
+
+        // division
+        int res4 = op1 / op2; // 10/0
+        System.out.println("Division "+ res4);
+    }
+
+    public void demo2() {
+        int[] arr = { 89, 90, 78 };
+        System.out.println(arr[9]);
+    }
+
+    public void demo3() {
+        String str = null;
+        str.length();
+    }
+
+    public void demo4() {
+        Class.forName("CarUtil"); // loads the class
+    }
+}

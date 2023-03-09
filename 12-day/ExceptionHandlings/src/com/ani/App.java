d9a5733f8294efc99cd409f5083f46403976eb0d
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-09 14:54:17 +0530

    exceptions : custom exceptions

--- 12-day/ExceptionHandlings/src/com/ani/App.java
+++ 12-day/ExceptionHandlings/src/com/ani/App.java
@@ -5,18 +5,30 @@ import com.ani.custom.UserInputting;
 import com.ani.custom.WrongFileFormatException;
 
 public class App {
-    public static void main(String[] args) {
+
+    public static void demo1() {
         UserInputting ip = new UserInputting();
 
         try {
             int age = ip.askForUserAge();
         } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
         }
 
         try {
             ip.createUserFromFile("abc.txt");
         } catch (WrongFileFormatException e) {
             e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        UserInputting ip = new UserInputting();
+        try {
+            ip.askForUserAge();      
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- 12-day/ExceptionHandlings/src/com/ani/custom/WrongFileFormatException.java
+++ 12-day/ExceptionHandlings/src/com/ani/custom/WrongFileFormatException.java
@@ -2,6 +2,9 @@ package com.ani.custom;
 
 public class WrongFileFormatException extends Exception {
 
+    private String cause; 
+    private String remedy;   
+
     public WrongFileFormatException(String message) {
         super(message);
     }

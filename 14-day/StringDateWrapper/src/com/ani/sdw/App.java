eba499837de3f08fb2a154d82b71748b22847d4f
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-11 13:23:20 +0530

    String : creation and equality

---
+++ 14-day/StringDateWrapper/src/com/ani/sdw/App.java
@@ -0,0 +1,11 @@
+package com.ani.sdw;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("🔥 String, Date, Warapper 🔥");
+
+        StringDemos sd = new StringDemos();
+        sd.demo2();
+    }
+}
---
+++ 14-day/StringDateWrapper/src/com/ani/sdw/StringDemos.java
@@ -0,0 +1,23 @@
+package com.ani.sdw;
+
+public class StringDemos {
+
+    public void demo1() {
+        String nm = "abc";
+        System.out.println(nm.hashCode());
+        nm = nm + "pqr" ;
+        System.out.println(nm.hashCode());
+    }
+
+    public void demo2() {
+        String nm = "abc"; // created memory pool
+
+        String ab = new String("abc"); // created by new keyword
+        System.out.println(" NM Equals AB = " + nm.equals(ab));
+        System.out.println(" NM == AB " + (nm == ab));
+
+        String pp = "abc"; // created memory pool
+        System.out.println("PP Equals NM = " + pp.equals(nm));
+        System.out.println("PP == NM " + (pp == nm));
+    }
+}

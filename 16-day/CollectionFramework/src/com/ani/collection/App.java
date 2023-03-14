21a2e02a519200274fe5c5ff2d498f275ff79ed6
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-14 10:56:48 +0530

    collection : interface

---
+++ 16-day/CollectionFramework/src/com/ani/collection/App.java
@@ -0,0 +1,14 @@
+package com.ani.collection;
+public class App {
+
+    public static void collectionDemo() {
+        CollectionDemo cd = new CollectionDemo();
+        cd.demo2();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("🔥 Collection Framework 🔥");
+
+        collectionDemo();
+    }
+}
---
+++ 16-day/CollectionFramework/src/com/ani/collection/CollectionDemo.java
@@ -0,0 +1,59 @@
+package com.ani.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class CollectionDemo {
+
+    public void demo1() {
+        String[] arr = {};
+    }
+
+    public void demo2() {
+        Collection<String> coll = new ArrayList<>();
+        coll.add("abc");
+        coll.add("pqr");
+        coll.add("xyz");
+        coll.add("xyz");
+        coll.add("xyz");
+        coll.add("lmn");
+        coll.add(null);
+        coll.add("tuv");
+        coll.add(null);
+
+        // Iterating over the collection using iterator
+        Iterator<String> it = coll.iterator();
+        while(it.hasNext()) {
+            String el = it.next();
+            System.out.println(el);
+        }
+
+        // Iterating over the collection using enhanced for
+        for(String el : coll) {
+            System.out.println(el);
+        }
+
+        // unfortunately we dont have a method to get element at ith location
+
+        // remove perticular element
+        coll.remove("xyz");
+
+        for(String el : coll) {
+            System.out.println(el);
+        }
+
+        String xyzSts =  coll.contains("xyz") ? "It contains xyz" : "It does not contains xyz";
+        System.out.println(xyzSts);
+
+        Collection<String> subColl = new ArrayList<>();
+        subColl.add("xyz");
+        subColl.add("790");
+
+        coll.addAll(subColl);
+        coll.removeAll(subColl);
+        coll.containsAll(subColl);
+        coll.retainAll(subColl);
+    }
+}

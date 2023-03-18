a093d24745cdc4da36a7b7fb38c7554265ac0019
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-18 13:14:28 +0530

    functional interfcaes : Function

--- 20-day/FunctionalInterfaces/src/com/ani/fi/App.java
+++ 20-day/FunctionalInterfaces/src/com/ani/fi/App.java
@@ -2,6 +2,18 @@ package com.ani.fi;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("🔥 Functional Interface 🔥");
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        
+        double d = 90.89d;
+        int a = 10;
+
+        // big = small
+        double dd = a;
+
+        // small = big
+        int aa = d;
     }
 }
--- 20-day/FunctionalInterfaces/src/com/ani/fi/FiDemos.java
+++ 20-day/FunctionalInterfaces/src/com/ani/fi/FiDemos.java
@@ -1,8 +1,11 @@
 package com.ani.fi;
 
+import java.util.function.Function;
+
 public class FiDemos {
     public void demo1() {
         Clickable clk = () -> 10; // ✅ function are assigned to variables
+        clk.click();
     }
 
     public Clickable demo2() { // ✅ functions can be returned from another function
@@ -15,5 +18,28 @@ public class FiDemos {
 
     public void demo4() {
         demo3( () -> 10 ); // ✅ functions can be passed as parameters to another function
+    }
+
+    public void demo5() {
+
+        Function<Integer, String> fn1 = (Integer day) -> {
+            if(day == 1) return "MON";
+            if(day == 2) return "TUE";
+            if(day == 3) return "WED";
+            if(day == 4) return "THU";
+            if(day == 5) return "FRI";
+            if(day == 6) return "SAT";
+            if(day == 7) return "SUN";
+            return "Invalid Day";
+        };
+        
+        System.out.println("Day 5 " + fn1.apply(5));
+        System.out.println("Day 500 " + fn1.apply(500));
+        // System.out.println("Day 500 " + fn1.apply(89.90));
+
+        Function<Boolean, String> fn2 = (Boolean a) -> a ? "ABC" : "abc";
+        System.out.println("Fn2 " + fn2.apply(true));
+        System.out.println("Fn2 " + fn2.apply(false));
+
     }
 }

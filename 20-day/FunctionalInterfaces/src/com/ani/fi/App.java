13305db6d3963f3eb1a5ed59aef96a85aa9c03ec
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-18 13:28:00 +0530

    functional interface : doubt clearing

--- 20-day/FunctionalInterfaces/src/com/ani/fi/App.java
+++ 20-day/FunctionalInterfaces/src/com/ani/fi/App.java
@@ -1,8 +1,8 @@
 package com.ani.fi;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("🔥 Functional Interface 🔥");
+
+    public static void basicsExplanation() {
 
         System.out.println(Double.MAX_VALUE);
         System.out.println(Integer.MAX_VALUE);
@@ -13,7 +13,18 @@ public class App {
         // big = small
         double dd = a;
 
-        // small = big
-        int aa = d;
+        // small != big
+        // int aa = d;
+    }
+
+    public static void functionDemo() {
+
+        FiDemos fd = new FiDemos();
+        fd.demo5();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("🔥 Functional Interface 🔥");
+        functionDemo();
     }
 }
--- 20-day/FunctionalInterfaces/src/com/ani/fi/FiDemos.java
+++ 20-day/FunctionalInterfaces/src/com/ani/fi/FiDemos.java
@@ -1,5 +1,6 @@
 package com.ani.fi;
 
+import java.util.Scanner;
 import java.util.function.Function;
 
 public class FiDemos {
@@ -22,7 +23,7 @@ public class FiDemos {
 
     public void demo5() {
 
-        Function<Integer, String> fn1 = (Integer day) -> {
+        Function<Integer, String> fn1 = (day) -> {
             if(day == 1) return "MON";
             if(day == 2) return "TUE";
             if(day == 3) return "WED";
@@ -33,13 +34,21 @@ public class FiDemos {
             return "Invalid Day";
         };
         
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("👉 Enter Day : ");
+        int dy = sc.nextInt();
+        System.out.println("Day  " + dy +" : " + fn1.apply(dy));
+
         System.out.println("Day 5 " + fn1.apply(5));
         System.out.println("Day 500 " + fn1.apply(500));
         // System.out.println("Day 500 " + fn1.apply(89.90));
 
-        Function<Boolean, String> fn2 = (Boolean a) -> a ? "ABC" : "abc";
+        Function<Boolean, String> fn2 = (Boolean jvhjvhfhgj) -> jvhjvhfhgj ? "ABC" : "abc";
         System.out.println("Fn2 " + fn2.apply(true));
         System.out.println("Fn2 " + fn2.apply(false));
 
     }
+
+    public void demo6(int kjhkjhgjkgjkh) { }
 }

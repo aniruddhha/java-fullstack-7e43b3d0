8ea67c9394e941cf3f5e7ec9a04f053372f3ce90
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-20 14:54:22 +0530

    coding-questions : more efficint solution

--- solutions-coding-questions/General/src/con/ani/solutions/collections/ContainsDuplicate.java
+++ solutions-coding-questions/General/src/con/ani/solutions/collections/ContainsDuplicate.java
@@ -6,15 +6,16 @@ import java.util.Set;
 
 public class ContainsDuplicate {
     
-    public boolean containsDuplicates(Integer[] nums) {
+    public boolean containsDuplicates(Integer[] nums) { // eff = 1 + n + 1 + 1 = 3 + n = n
 
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(Arrays.asList(nums)); // 1
+        return set.size() != nums.length; // 1
 
-        for(int num : nums) {
-            if(set.contains(num)) return true;
-            set.add(num);
-        }
+        // for(int num : nums) { // n
+        //     if(set.contains(num)) return true; // 1
+        //     set.add(num); // 1
+        // }
 
-        return false;
+        // return false; //o(n)
     }
 }

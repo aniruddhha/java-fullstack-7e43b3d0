c6a203023cd877af178591207fbc38d007a45160
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-20 11:43:23 +0530

    java 8 : stream operations

--- 21-day/StreamApi/src/com/ani/stream/App.java
+++ 21-day/StreamApi/src/com/ani/stream/App.java
@@ -6,7 +6,7 @@ public class App {
         System.out.println("🔥 Streams API 🔥");
 
         StreamDemos sd = new StreamDemos();
-        sd.demo2();
+        sd.demo4();
 
     }
 }
--- 21-day/StreamApi/src/com/ani/stream/StreamDemos.java
+++ 21-day/StreamApi/src/com/ani/stream/StreamDemos.java
@@ -3,17 +3,15 @@ package com.ani.stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StreamDemos {
 
-    private String[] arr = { "abc", "xyz", "pqb", "wet", "qza", "opm", "nert", "bbc" };
-
-    private List<String> names = new ArrayList<>(Arrays.asList(arr));
-
     public void demo1() {
         
+        String[] arr = { "abc", "xyz", "pqb", "wet", "qza", "opm", "nert", "bbc" };
+        List<String> names = new ArrayList<>(Arrays.asList(arr));
+
         List<String> processed = new ArrayList<>(); // collected output here
 
         for (String nm : names) {
@@ -27,10 +25,40 @@ public class StreamDemos {
     }
 
     public void demo2() {
-        List<String> processed =  names.stream()
+            
+        String[] arr = { "abc", "xyz", "pqb", "wet", "qza", "opm", "nert", "bbc" };
+        List<String> names = new ArrayList<>(Arrays.asList(arr));
+
+        List<String> processed =  names.stream() //names.parallelStream() // 1. create the stream
+            //2. Perform an Intermidiate operation i.e filter
             .filter(el -> el.contains("a") || el.contains("b")) // processing
+            // 3. Collect using terminal operation
             .collect(Collectors.toList()); // collected the output
 
+        System.out.println(names);
+        System.out.println(processed);
+    }
+
+    public void demo3() {
+        String[] arr = { "abc", "xyz", "pqb", "wet", "qza", "opm", "nert", "bbc" };
+        List<String> names = new ArrayList<>(Arrays.asList(arr));
+
+        List<String> processed = new ArrayList<>();
+        for ( String nm : names ) {
+            processed.add(nm.toUpperCase());
+        }   
+
+        System.out.println(names);
+        System.out.println(processed);
+    }
+
+    public void demo4() {
+        String[] arr = { "abc", "xyz", "pqb", "wet", "qza", "opm", "nert", "bbc" };
+        List<String> names = new ArrayList<>(Arrays.asList(arr));
+
+        List<String> processed = names.stream()
+                                        .map( el -> el.toUpperCase() )
+                                        .collect(Collectors.toList());
         System.out.println(names);
         System.out.println(processed);
     }

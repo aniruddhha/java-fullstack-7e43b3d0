336a315bf9992aa6edef74536c4557910dea5c5a
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-23 16:13:23 +0530

    JUnit : few more test cases

--- 23-day/testing-app/src/test/java/com/ani/testing/CalcUtilTest.java
+++ 23-day/testing-app/src/test/java/com/ani/testing/CalcUtilTest.java
@@ -1,12 +1,16 @@
 package com.ani.testing;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.ani.testing.util.CalcUtil;
 
 public class CalcUtilTest {
     
+    @DisplayName("Test for normal inputs")
     @Test
     public void checkForSimpleInput() {
 
@@ -20,6 +24,7 @@ public class CalcUtilTest {
         Assertions.assertEquals(expectedOp, actualOp);
     }
 
+    
     @Test
     public void checkCalcInCorrect() {
         int num1 = 10;
@@ -53,5 +58,15 @@ public class CalcUtilTest {
             CalcUtil cu = new CalcUtil();
             cu.calc(num1, num2);
         });
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "10, 10, 20", "18, 1, 19", "16, 5, 21" })
+    public void paramaterizedTestDemo(int num1, int num2, int expectedOp) {
+
+        CalcUtil cu = new CalcUtil();
+        int actualOp = cu.calc(num1, num2);
+
+        Assertions.assertEquals(expectedOp, actualOp);
     }
 }

cb0c86186c6df66760d43dbde6ea60713fdd99e8
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-29 10:37:52 +0530

    javascript : intro and simple code

---
+++ 28-day/jsbasics/app.js
@@ -0,0 +1,5 @@
+const num1 = 10
+const num2 = 20
+console.log('👉 Addition of  ' + num1 + ' and '+ num2 + ' is ' + (num1 + num2))
+// this feature is newly introduced in ES6
+console.log(`🔥 👉 Addition is ${num1} and ${num2} is ${num1 + num2}`)

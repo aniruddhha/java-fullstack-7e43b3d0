47d782de18cb96b4c49a0b464ef20bba8f42db5d
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-29 12:35:21 +0530

    loops : for, while and do-while

---
+++ 28-day/jsbasics/arrays-loops.js
@@ -0,0 +1,23 @@
+const nums = [1, 4, 5, 2, 3 ,9, 10]
+
+console.log(`0th Element ${nums[0]}`)
+console.log(`1st Element ${nums[1]}`)
+
+console.log(`🔥 For Loop 🔥`)
+for (let i = 0; i < nums.length; i++ ) {
+    console.log(`Index ${i} -> Element ${nums[i]}`)
+}
+
+console.log(`🔥 While Loop 🔥`)
+let i = 0
+while( i < nums.length) {
+    console.log(`Index ${i} -> Element ${nums[i]}`)
+    i++
+}
+
+console.log(`🔥 Do While Loop 🔥`)
+let j = 0;
+do {
+    console.log(`Index ${j} -> Element ${nums[j]}`)
+    j++
+} while( j < nums.length )

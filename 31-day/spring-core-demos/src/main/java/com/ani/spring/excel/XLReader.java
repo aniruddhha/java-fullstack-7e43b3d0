dbfa803709c0bfee97f24bf272bb75ba4f4597b7
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-03 14:47:35 +0530

    spring : setter, constructor and field level injection

--- 31-day/spring-core-demos/src/main/java/com/ani/spring/excel/XLReader.java
+++ 31-day/spring-core-demos/src/main/java/com/ani/spring/excel/XLReader.java
@@ -6,8 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class XLReader {
     
-    @Autowired // filed level injection
+    // @Autowired // filed level injection
     private XLFormatter formatter;
+
+    // @Autowired // constructor level injection
+    // public XLReader(XLFormatter formatter) {
+    //     this.formatter = formatter;
+    // }
+
+    @Autowired // setter level injection
+    public void setFormatter(XLFormatter formatter) {
+        this.formatter = formatter;
+    }
 
     public void setReadLimit(int limit) {
         System.out.println("XLReader : Set Read Limit " + limit);

eeabdcbd7fa9ad34a969913bea948aad2db955dc
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-04 16:03:14 +0530

    spring mvc : @RequestParam form submisison

--- 32-day/spring-web-demos/src/main/java/com/ani/web/controller/UserController.java
+++ 32-day/spring-web-demos/src/main/java/com/ani/web/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ani.web.entity.User;
@@ -63,6 +64,11 @@ public class UserController {
         mv.addObject("eml", user.getEmail());
         
         return mv;
+    }
+
+    @PostMapping("/createv2")
+    public String createUserV2(@RequestParam Integer id, @RequestParam String name, @RequestParam String email, @RequestParam String mobile) {
+        return "success";
     }
 
     @GetMapping("/show")

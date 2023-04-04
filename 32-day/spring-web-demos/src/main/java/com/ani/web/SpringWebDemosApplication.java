9e4b789159172a2716eb5bc28ac8b8e711893726
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-04 11:06:40 +0530

    spring web : mvc demo app

---
+++ 32-day/spring-web-demos/src/main/java/com/ani/web/SpringWebDemosApplication.java
@@ -0,0 +1,13 @@
+package com.ani.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDemosApplication.class, args);
+	}
+
+}
---
+++ 32-day/spring-web-demos/src/test/java/com/ani/web/SpringWebDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

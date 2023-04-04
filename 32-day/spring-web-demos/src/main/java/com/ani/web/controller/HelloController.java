4434ba4508d0210240f43c9b2d88a985c374d7a3
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-04 11:48:34 +0530

    spring mvc : explanation

--- 32-day/spring-web-demos/src/main/java/com/ani/web/controller/HelloController.java
+++ 32-day/spring-web-demos/src/main/java/com/ani/web/controller/HelloController.java
@@ -3,13 +3,17 @@ package com.ani.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+@RequestMapping("/demo") 
+public class HelloController { // http://localhost:8080/demo
     
-    @GetMapping("/hlo")
-    public String sayHello(Model model) {
+    @GetMapping("/hlo") 
+    public String sayHello(Model model) { // http://localhost:8080/demo/hlo
+
         model.addAttribute("name", "abc");
         return "hello";
     }
+    //   /WEB-INF/jsp/hello.jsp
 }
---
+++ 32-day/spring-web-demos/src/main/webapp/WEB-INF/jsp/hi.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Hi</title>
+</head>
+<body>
+    <h1>This this from Hi Controller</h1>
+</body>
+</html>

4ad235b79008ed8396e8fdbc36df87e1cac12351
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-05 11:40:30 +0530

    spring boot : application.properties file

--- 33-day/spring-boot-understanding/src/main/java/com/ani/boot/service/CarService.java
+++ 33-day/spring-boot-understanding/src/main/java/com/ani/boot/service/CarService.java
@@ -1,5 +1,6 @@
 package com.ani.boot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ani.boot.entity.Car;
@@ -7,12 +8,16 @@ import com.ani.boot.entity.Car;
 @Service
 public class CarService {
     
+    @Value("${ws}")
+    private String website;
+
     public void createNewCar(Long id, String make, String model) {
 
         Car car = new Car();
         car.setId(id);
         car.setMake(make);
         car.setModel(model);
+        System.out.println("Car Company Website: " + website);
         System.out.println(car.toString());
     }
 }

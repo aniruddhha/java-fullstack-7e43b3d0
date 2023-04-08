2f578fa98cefb3034230eb42d8b6475d59fbfa45
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-08 10:57:03 +0530

    web flux : simple demo

---
+++ 35-day/webflux-demo/src/main/java/com/ani/webflux/controller/RangeController.java
@@ -0,0 +1,21 @@
+package com.ani.webflux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class RangeController {
+    
+    @GetMapping(value = "/numbers")
+    public Flux<Integer> generateNumbers() {
+        return Flux.range(1, 100) // Publisher emits 1 to 10
+                   .log() // Log each event for demonstration purposes
+                   .map( i -> i * 2) // // Processor multiplies each number by 2
+                   .doOnRequest(n -> System.out.println("Requesting " + n +" items")) // Log number of requested items
+                   .doOnNext(n -> System.out.println("Processing " + n)) // // Log each processed item
+                   .limitRate(3); // // Backpressure: limit rate to 3 items per second
+
+    }
+}

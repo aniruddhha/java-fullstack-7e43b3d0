ebec45047070aebc0fb88b722f02ef59b9af4f65
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-08 16:09:08 +0530

    safe commmit

--- 35-day/reactive-mongo-demo/src/main/java/com/ani/reactive/mongo/ReactiveMongoDemoApplication.java
+++ 35-day/reactive-mongo-demo/src/main/java/com/ani/reactive/mongo/ReactiveMongoDemoApplication.java
@@ -12,28 +12,29 @@ public class ReactiveMongoDemoApplication {
 	public static void main(String[] args) throws InterruptedException {
 		ApplicationContext ctx = SpringApplication.run(ReactiveMongoDemoApplication.class, args);
 
-        CountDownLatch latch = new CountDownLatch(1);
+		CountDownLatch latch = new CountDownLatch(2);
 
 		ItemRepository repository = ctx.getBean(ItemRepository.class);
-		
+
 		Item item = new Item(10L, "abc", "aaa", 456.90);
 
 		repository.save(item)
-				.doOnNext( savedItem ->  { 
-					System.out.println("🔥 Save Completed 🔥"); 
-					System.out.println(savedItem); 
+				.doOnNext(savedItem -> {
+					System.out.println("🔥 Save Completed 🔥");
+					System.out.println(savedItem);
 					System.out.println("👉 One");
-				} )
+					// latch.countDown();
+				})
 				.doOnError(err -> System.out.println(err))
 				.subscribe();
 
-
 		repository.findAll()
-				.doOnNext( itm ->  { 
-					System.out.println("🔥 FindAll Completed 🔥"); 
-					System.out.println(itm); 
+				.doOnNext(itm -> {
+					System.out.println("🔥 FindAll Completed 🔥");
+					System.out.println(itm);
+					// latch.countDown();
 				})
-				.doOnError( err -> System.out.println(err) )
+				.doOnError(err -> System.out.println(err))
 				.subscribe();
 
 		System.out.println("👉 Two");

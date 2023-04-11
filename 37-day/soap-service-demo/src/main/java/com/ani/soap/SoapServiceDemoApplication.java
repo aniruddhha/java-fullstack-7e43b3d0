e351bfb911da76aaa3cdb0f4b048422487db7a83
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-11 10:15:05 +0530

    soap: pom, xsd file

---
+++ 37-day/soap-service-demo/src/main/java/com/ani/soap/SoapServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ani.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServiceDemoApplication.class, args);
+	}
+
+}
---
+++ 37-day/soap-service-demo/src/test/java/com/ani/soap/SoapServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

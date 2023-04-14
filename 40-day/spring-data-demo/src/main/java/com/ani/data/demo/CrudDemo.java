c3f0d93b5af25f397eb20801619361437917dc5d
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-14 11:46:44 +0530

    refractoring the code

---
+++ 40-day/spring-data-demo/src/main/java/com/ani/data/demo/CrudDemo.java
@@ -0,0 +1,50 @@
+package com.ani.data.demo;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.ani.data.domain.Invoice;
+import com.ani.data.domain.repository.InvoiceRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component
+public class CrudDemo {
+    
+    private final InvoiceRepository repository;
+
+    public void demo1() {
+        Invoice invoice1 = new Invoice();
+		invoice1.setId(2L);
+		invoice1.setClient("pqr");
+		invoice1.setAmt(8934.3d);
+		invoice1.setInvDt(LocalDate.now());
+
+		System.out.println("🟢 Creating New Invoice 🟢");
+		repository.save(invoice1);
+		System.out.println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉");
+
+		System.out.println("🟢 Reading All Invoices 🟢");
+		Iterable<Invoice> invoices = repository.findAll();
+		invoices.forEach( inv -> System.out.println(inv));
+		System.out.println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉");
+
+		System.out.println("🟢 Reading Single Invoive 🟢");
+		Optional<Invoice> opInv1 = repository.findById(2L);
+		Invoice inv2 = opInv1.orElseThrow(() -> new RuntimeException("❌ Invlaid Id"));
+		System.out.println(inv2);
+		System.out.println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉");
+
+		System.out.println("🟢 Updating Invoive 🟢");
+		Invoice invoice3 = new Invoice();
+		invoice3.setId(2L);
+		invoice3.setClient("ddd");
+		invoice3.setAmt(1234.456d);
+		invoice3.setInvDt(LocalDate.of(2022, 01, 30));
+		repository.save(invoice3);
+		System.out.println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉");
+    }
+}

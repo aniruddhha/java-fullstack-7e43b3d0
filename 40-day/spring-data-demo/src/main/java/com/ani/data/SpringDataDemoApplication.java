811ff5e300d43397e9f6cb1651b8c4d4cd321573
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-14 10:22:58 +0530

    spring data : repository setup

---
+++ 40-day/spring-data-demo/src/main/java/com/ani/data/SpringDataDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ani.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataDemoApplication.class, args);
+	}
+
+}
---
+++ 40-day/spring-data-demo/src/test/java/com/ani/data/SpringDataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

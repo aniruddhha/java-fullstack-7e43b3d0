67934d54e11f9f1836bf18cc2f96484efd247f8a
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-17 16:07:42 +0530

    HQL

--- 42-day/hibernate-demo/src/main/java/com/ani/orm/HibernateDemoApplication.java
+++ 42-day/hibernate-demo/src/main/java/com/ani/orm/HibernateDemoApplication.java
@@ -1,7 +1,10 @@
 package com.ani.orm;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +14,8 @@ import com.ani.orm.util.HibernateUtil;
 @SpringBootApplication
 public class HibernateDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernateDemoApplication.class, args);
 
+	public static void demo1() {
 		Car car = new Car();
 		car.setId(50L);
 		car.setName("abc");
@@ -25,6 +27,29 @@ public class HibernateDemoApplication {
 		txn.commit();
 
 		session.close();
+	}
+
+	public static void demo2() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		// Transaction txn = session.beginTransaction();
+		Query<Car> query1 = session.createQuery("from Car", Car.class);
+		List<Car> cars1 = query1.getResultList();
+		cars1.forEach(System.out::println);
+		// txn.commit();	
+		// session.close();
+
+		Query<Car> query2 = session.createQuery("from Car where id = :id", Car.class);
+		query2.setParameter("id", 1L);
+		Car car1 = query2.getSingleResult();
+		System.out.println(car1);
+
+		session.close();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDemoApplication.class, args);
+
+		demo2();
 	}
 
 }
--- 42-day/hibernate-demo/src/main/java/com/ani/orm/domain/Car.java
+++ 42-day/hibernate-demo/src/main/java/com/ani/orm/domain/Car.java
@@ -8,7 +8,9 @@ import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Setter
 @Getter
 @Entity

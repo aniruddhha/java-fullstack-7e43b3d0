af60282a5c9eaf39ab0583b83dca9c1bb969fb1f
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-17 12:53:01 +0530

    association mapping : one to many, one to one

--- 42-day/association-mappings/src/main/java/com/ani/mapping/domain/ContactDetails.java
+++ 42-day/association-mappings/src/main/java/com/ani/mapping/domain/ContactDetails.java
@@ -1,15 +1,27 @@
 package com.ani.mapping.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
 
-// @Setter
-// @Getter
-// @Entity
+@Setter
+@Getter
+@Entity
 public class ContactDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String mobile;
     private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
 }
--- 42-day/association-mappings/src/main/java/com/ani/mapping/domain/Person.java
+++ 42-day/association-mappings/src/main/java/com/ani/mapping/domain/Person.java
@@ -6,9 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -21,10 +25,18 @@ public class Person {
     private String name;
     private LocalDate dob;
 
+    // bi-directional
+    @OneToOne(mappedBy = "person" )
+    private IdentityDoc identityDoc;
+
+    @OneToMany(mappedBy = "person")
+    private List<ContactDetails> contactDetails;
+
 }
 
 // one person can have one unique identity
 // one identity can belongs to one person
+// bi-directionality is an optional, depnds on business need
 
 /* 
  

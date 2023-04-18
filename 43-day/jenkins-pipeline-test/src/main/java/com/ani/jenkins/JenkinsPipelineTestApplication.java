af4f564a19e3e98816945482536ad5f2b33c6445
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-18 12:06:49 +0530

    jenkins test

---
+++ 43-day/jenkins-pipeline-test/src/main/java/com/ani/jenkins/JenkinsPipelineTestApplication.java
@@ -0,0 +1,13 @@
+package com.ani.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineTestApplication.class, args);
+	}
+
+}
---
+++ 43-day/jenkins-pipeline-test/src/test/java/com/ani/jenkins/JenkinsPipelineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

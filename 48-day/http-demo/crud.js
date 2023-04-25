2628cd0e048b5229b58bd81a9e1c1ab6edf1448a
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-25 15:43:09 +0530

    http : ulr mapping. methods and responses

---
+++ 48-day/http-demo/crud.js
@@ -0,0 +1,46 @@
+const http = require('node:http')
+
+const server = http.createServer((req, res) => {
+    // const url = req.url
+    // const method = req.method
+
+    const { url, method } = req
+
+    if(method === 'GET' && url === '/') {
+        // Handle GET request for root path
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'GET request received for root path' }));
+    } else if(method === 'POST' && url === '/') {
+        // Handle POST request for root path
+
+        let reqData = '';
+        req.on('data', chunk => reqData += chunk )
+        req.on('end', () => {
+            const json = JSON.parse(reqData);
+            console.log('Received JSON:', json);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'JSON received successfully', bdy : json }));
+        })
+    } else if (method === 'PUT' && url === '/users') {
+        // Handle PUT request for /users path
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'PUT request received for /users path' }));
+    }  else if (method === 'DELETE' && url === '/users') {
+        // Handle DELETE request for /users path
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'DELETE request received for /users path' }));
+    } else {
+         // Handle requests with unknown method or URL path
+         res.writeHead(404, { 'Content-Type': 'text/plain' });
+         res.end('404 Not Found');
+    }
+})
+
+server.listen(3000, '127.0.0.1', (err) => {
+    if(err) {
+        console.log(`❌ There is an error`)    
+        console.log(err)
+        return
+    }
+    console.log(`✅ Listening on 3000 port`)
+})

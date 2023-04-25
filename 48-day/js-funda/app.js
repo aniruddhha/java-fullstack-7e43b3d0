d4ceb24901a35d822b5d11a45890896cb86f9413
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-25 10:52:15 +0530

    nodejs project

--- 48-day/js-funda/app.js
+++ 48-day/js-funda/app.js
@@ -1,1 +1,13 @@
 console.log('🎉 Welcome To NodeJs')
+
+const AsciiTable = require('ascii-table')
+const table = new AsciiTable('A Title')
+table
+  .setHeading('', 'Name', 'Age')
+  .addRow(1, 'Bob', 52)
+  .addRow(2, 'John', 34)
+  .addRow(3, 'Jim', 83)
+ 
+console.log(table.toString())
+
+

1b1d46f1e6202d7f5b8e692e4c8273aa1d157211
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-26 10:51:38 +0530

    nodejs : buffers and file handling

--- 49-day/buffers/index.js
+++ 49-day/buffers/index.js
@@ -1,4 +1,5 @@
 import { Buffer } from 'node:buffer'; // Es6 modules
+import * as fs from 'node:fs/promises';
 // const { Buffer } = require('node:buffer') // CommonJs module
 
 const buf1 = Buffer.alloc(10);
@@ -14,5 +15,14 @@ console.log(subBuf.toString())
 const buf3 = Buffer.from([1, 2, 3]);
 console.log(buf3)
 
+const buf4 = Buffer.from('hello')
+console.log(buf4)
+const buf5 = Buffer.from('world')
+console.log(buf5)
+const buf6 = Buffer.concat([buf4, buf5])
+console.log(buf6)
 
-
+fs.writeFile('./text.txt', buf6)
+    .then( () => console.log('Data Written Successfully') )
+    .catch(err => console.log(err))
+fs.readFile('./text.txt').then((dt) => console.log(dt.toString()))

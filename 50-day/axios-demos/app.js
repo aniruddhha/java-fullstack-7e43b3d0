57781a65fdeaeb9ba79a461529134314ab3a8f60
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-27 13:06:06 +0530

    axios : crud

--- 50-day/axios-demos/app.js
+++ 50-day/axios-demos/app.js
@@ -12,3 +12,24 @@ axios.get('https://reqres.in/api/users?page=2')
   .finally(function () {
     // always executed
   });
+
+const data = { 
+    name : 'abc',
+    job: 'aaa'
+}
+
+axios.post('https://reqres.in/api/users', data)
+    .then( res  => console.log(res.data))
+    .catch(err => console.log(err))
+
+axios.delete('https://reqres.in/api/users/2')
+    .then( res => console.log(res.status) )
+    .catch(err => console.log(err))
+
+const updatedData = { 
+    name : 'abc',
+    job: 'sss'
+}
+axios.put('https://reqres.in/api/users/2', updatedData)
+    .then( res => console.log(res.data) )
+    .catch(err => console.log(err))

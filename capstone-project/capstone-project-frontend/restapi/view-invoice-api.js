1ff60cdb2c7f5228c6d69f7f741adccb9e8a57ef
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-28 16:40:09 +0530

    capstone-project: view variation

--- capstone-project/capstone-project-frontend/restapi/view-invoice-api.js
+++ capstone-project/capstone-project-frontend/restapi/view-invoice-api.js
@@ -10,15 +10,26 @@ function apiGetInvoiceDetails() {
 
     axios.get(`http://localhost:8080/invoice/${id}`)
         .then(httpReponse => httpReponse.data)
-        .then(data => populateDetails(data.bd))
+        .then(data => populateTableDetails(data.bd))
         .catch(err => console.log(err))
 }
 
 function populateDetails({ id, client, amt, invDt }) {
+    // populating invoice details without table
     document.getElementById("invId").innerHTML = `<strong> Id </strong> : ${id}`
     document.getElementById("client").innerHTML = `<strong> Client </strong> : ${client}`
     document.getElementById("amt").innerHTML = `<strong> Amount </strong> : ${amt}`
-    document.getElementById("invDt").innerHTML= `<strong> Date </strong> : ${invDt}`
+    document.getElementById("invDt").innerHTML = `<strong> Date </strong> : ${invDt}`
+}
+
+function populateTableDetails({ id, client, amt, invDt }) {
+    // populating invoice details inside a table
+    const table = document.getElementById('tableDetails')
+    const row = table.insertRow()
+    row.insertCell(0).innerHTML = id
+    row.insertCell(1).innerHTML = client
+    row.insertCell(2).innerHTML = amt
+    row.insertCell(3).innerHTML = invDt 
 }
 
 apiGetInvoiceDetails()

e7ef358bd203498771dce90c3ec7b4d1210d0ea9
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-28 10:52:37 +0530

    nodejs: mysql CRUD

--- 51-day/node-mysql-connectivity/index.js
+++ 51-day/node-mysql-connectivity/index.js
@@ -7,9 +7,77 @@ const connection = mysql.createConnection({
 })
 
 connection.connect(err => {
-    if(err) {
+    if (err) {
         console.log(`❌ Problem In Connectivity`)
         return
     }
+
     console.log(`✅ Connected Successfully`)
+
+    connection.query("CREATE DATABASE nodejsdb", (err, result) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log("✅ Database created");
+    });
+
+    connection.query("use nodejsdb", (err, result) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log("✅ Database Selected");
+    });
+
+    const createSql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    connection.query(createSql, (err, result) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log("✅ Table created");
+    });
+
+    const insertSql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    connection.query(insertSql, (err, result) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log("✅ 1 record inserted");
+    });
+
+    connection.query("SELECT * FROM customers", (err, result, fields) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log(result);
+    });
+
+    const updateSql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Highway 37'";
+    connection.query(updateSql, (err, result) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log(result.affectedRows + " record(s) updated");
+    });
+
+    const deleteSql = "DELETE FROM customers WHERE address = 'Highway 37'";
+    connection.query(deleteSql, (err, result) => {
+        if (err) {
+            console.log(`❌ Problem In Connectivity`)
+            console.log(err)
+            return
+        }
+        console.log("Number of records deleted: " + result.affectedRows);
+    });
 })

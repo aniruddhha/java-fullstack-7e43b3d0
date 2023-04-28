03538fa178d01a30a196774d085e6ae645db3b73
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-28 13:05:59 +0530

    nodejs : mongo revised code for updated driver

--- 51-day/node-mongo-connectivity/index.js
+++ 51-day/node-mongo-connectivity/index.js
@@ -1,69 +1,72 @@
 // console.log(require('mongodb'))
 
 function moduleImportExportRevision() {
-    const obj = { 
-        MongoClinent : 'abc',  
-        MongoError : 'error'
+    const obj = {
+        MongoClinent: 'abc',
+        MongoError: 'error'
     }
-    
+
     const MongoCl = obj.MongoClinent
     const MongoErr = obj.MongoError
-    
+
     const { MongoClinent, MongoError } = obj
 }
 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'nodejsdb';
+const url = 'mongodb://127.0.0.1:27017/test';
 
-MongoClient.connect(url, (err, client) => {
-    if(err) {
-        console.log(`❌ Problem in connectivity`)
-        console.log(err)
-        return
+const client = new MongoClient(url, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
+});
 
-    const db = client.db(dbName);
+client.connect().then(() => {
+    console.log(`✅ Connected Successfully`)
+
+    const db = client.db('test');
+
     const collection = db.collection('cars');
 
-    collection.insertOne({a: 1}, (err, result) => {
-        if(err) {
-            console.log(`❌ Problem Inserting`)
-            console.log(err)
-            return
-        }
-    
-        console.log("Inserted document with _id: " + result.insertedId);
-    });
+    collection.insertOne({ a: 1 })
+    .then(result => {
+        console.log(`✅ Data Inserted Successfully`)
+        console.log(result)
+    }).catch(err => {
+        console.log(`❌ Problme in inserting data`)
+        console.log(err)
+    })
 
-    collection.find({}).toArray((err, docs) => {
-        if(err) {
-            console.log(`❌ Problem In Fetching`)
-            console.log(err)
-            return
-        }
+    // Find documents
+    collection.find({}).toArray().then(docs => {
         console.log("Found the following records:");
         console.log(docs);
-    });
+    }).catch(err => {
+        console.log(`❌ Problme in inserting data`)
+        console.log(err)
+    })
 
-    collection.updateOne({ a : 2 }, { $set: { b : 1 } }, (err, result) => {
-        if(err) {
-            console.log(`❌ Problem In Updating`)
+    // Update a document
+    collection.updateOne({ a: 2 }, { $set: { b: 1 } })
+        .then(result => {
+            console.log("✅ Updated the document");
+            console.log(result)
+        }).catch(err => {
+            console.log(`❌ Problme in inserting data`)
             console.log(err)
-            return
-        }
-        console.log("Updated the document");
-    });
-    
-    collection.deleteOne({ a : 3 }, (err, result) => {
-        if(err) {
-            console.log(`❌ Problem In Updating`)
-            console.log(err)
-            return
-        }
-        console.log("Deleted the document");
-    });
+        })
 
-    client.close();
-})
+    // Delete a document
+    collection.deleteOne({ a: 3 }).then(result => {
+        console.log("✅ Deleted the document");
+        console.log(result)
+    }).catch(err => {
+        console.log(`❌ Problme in inserting data`)
+            console.log(err)
+    })
+
+    // client.close();
+}).catch(console.log);

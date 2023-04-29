8b943443dab157a270764e16a1be2e3f562ef74b
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-29 11:35:00 +0530

    typeorm : crud

--- 52-day/typeorm-crud/index.js
+++ 52-day/typeorm-crud/index.js
@@ -16,22 +16,24 @@ dataSource.initialize().then(() => {
     console.log(`✅ Successfully Created`)
 
     var category1 = {
-        name: "TypeScript",
+        id: 1,
+        name: "Python",
     }
     var category2 = {
         name: "Programming",
     }
 
     const categoryRepository = dataSource.getRepository('Category')
-    // categoryRepository.save(category1).then(saved => {
-    //     console.log(`✅ Category Saved`)
-    // }).catch(console.log)
-    // categoryRepository.save(category2).then(saved => {
-    //     console.log(`✅ Category Saved`)
-    // }).catch(console.log)
+    categoryRepository.save(category1).then(saved => {
+        console.log(`✅ Category Saved`)
+    }).catch(console.log)
+    categoryRepository.save(category2).then(saved => {
+        console.log(`✅ Category Saved`)
+    }).catch(console.log)
 
-    categoryRepository.find().then(categories => console.log(categories)).catch(console.log)
+    // categoryRepository.find().then(categories => console.log(categories)).catch(console.log)
 
+    // categoryRepository.delete(2).then(res => console.log(`✅ Deleted Successfully`)).catch(console.log)
 }).catch(error => {
     console.log("❌ Error: ", error)
 })

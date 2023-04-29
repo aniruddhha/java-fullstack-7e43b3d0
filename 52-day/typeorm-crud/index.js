5008dedf04bfc5f58f5a88e3b2afd72762754868
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-29 11:51:30 +0530

    typeorm : query builders

--- 52-day/typeorm-crud/index.js
+++ 52-day/typeorm-crud/index.js
@@ -24,16 +24,25 @@ dataSource.initialize().then(() => {
     }
 
     const categoryRepository = dataSource.getRepository('Category')
-    categoryRepository.save(category1).then(saved => {
-        console.log(`✅ Category Saved`)
-    }).catch(console.log)
-    categoryRepository.save(category2).then(saved => {
-        console.log(`✅ Category Saved`)
-    }).catch(console.log)
+
+    // categoryRepository.save(category1).then(saved => {
+    //     console.log(`✅ Category Saved`)
+    // }).catch(console.log)
+    // categoryRepository.save(category2).then(saved => {
+    //     console.log(`✅ Category Saved`)
+    // }).catch(console.log)
 
     // categoryRepository.find().then(categories => console.log(categories)).catch(console.log)
 
     // categoryRepository.delete(2).then(res => console.log(`✅ Deleted Successfully`)).catch(console.log)
+
+    categoryRepository.query('select * from categories')
+                        .then(ctgs => console.log(ctgs))
+                        .catch(console.log)
+
+    const categories = categoryRepository.createQueryBuilder('Category').where('Category.name like :nm', {nm: '%P%'})
+    categories.getMany().then(ctgs => console.log(ctgs))
+    
 }).catch(error => {
     console.log("❌ Error: ", error)
 })

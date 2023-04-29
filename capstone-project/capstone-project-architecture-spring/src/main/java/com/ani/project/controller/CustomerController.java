575a78c90860000ce53c70195f97ddaa7055d3cb
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-04-29 17:07:07 +0530

    capstone project: improvised code

--- capstone-project/capstone-project-architecture-spring/src/main/java/com/ani/project/controller/CustomerController.java
+++ capstone-project/capstone-project-architecture-spring/src/main/java/com/ani/project/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.ani.project.service.CustomerService;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin
 @AllArgsConstructor
 @RequestMapping(value = "/customer")
 @RestController
--- capstone-project/capstone-project-frontend/restapi/create-invoice-api.js
+++ capstone-project/capstone-project-frontend/restapi/create-invoice-api.js
@@ -12,8 +12,44 @@ function apiCreateNewInvoice(invoice, form) {
                 .catch(err => console.log(err))
 }
 
+function apiCreateNewCustomerInvoive(invoice, form) {
+        const headers = {
+                'content-type' : 'application/json'
+        }
+
+        axios.post('http://localhost:8080/invoice/customer', invoice, { headers })
+                .then(res => {
+                        form.reset()
+                        showSuccessModal()
+                })
+                .catch(err => console.log(err))
+}
+
+
+function apiFindCustomersContaining(ss) {
+        const url = `http://localhost:8080/customer/${ss}`
+        axios.get(url)
+                .then(httpResponse => httpResponse.data)
+                .then(data => populateCustomerDataList(data))
+                .catch(err => console.log(err))
+
+}
+
 function setupForm() {
+
+        const searchCustomerIp = document.getElementById('ipCustomerSearch')
+        searchCustomerIp.onkeyup = ke => {
+                console.log(ke)
+               
+                const seachString = ke.target.value
+                console.log(seachString)
+
+                apiFindCustomersContaining(seachString)
+        }
+         
+
         const formInvoice = document.getElementById('formInvoice')
+
 
         formInvoice.onsubmit = ev => { // when form is submitted, this function would be called
                
@@ -24,13 +60,13 @@ function setupForm() {
 
                 const formData = new FormData(ev.target) // ev.target points to form tag in the html
 
-                for(let key of formData.keys()) {
-                        console.log(key) // this is mapped to name attribute of input tags
-                }
+                // for(let key of formData.keys()) {
+                //         console.log(key) // this is mapped to name attribute of input tags
+                // }
 
-                for(let val of formData.values()) {
-                        console.log(val) // this is mapped to name attribute of input tags
-                }
+                // for(let val of formData.values()) {
+                //         console.log(val) // this is mapped to name attribute of input tags
+                // }
 
                 // const invoice = { 
                 //         client : formData.get('client') ,
@@ -41,7 +77,8 @@ function setupForm() {
                 const invoice = Object.fromEntries(formData.entries()) // you are converting form data to js object
                 console.log(invoice)
 
-                apiCreateNewInvoice(invoice, formInvoice) // we are pass form object to reset the form on success
+                // apiCreateNewInvoice(invoice, formInvoice) // we are pass form object to reset the form on success
+                apiCreateNewCustomerInvoive(invoice, formInvoice)
         }
 }
 setupForm()
@@ -50,4 +87,17 @@ function showSuccessModal() {
         const myModalEl = document.getElementById('successModal');
         const modal = new bootstrap.Modal(myModalEl)
         modal.show()
+}
+
+function populateCustomerDataList(customers) {
+        console.log(customers)
+        const dataList = document.getElementById('datalistCustomers')
+        dataList.innerHTML = ''
+
+        for (const { id, name, mobile } of customers) {
+                const option = document.createElement('option')
+                option.value = id
+                option.text = `${name}, ${mobile}`
+                dataList.appendChild(option)
+        }
 }
